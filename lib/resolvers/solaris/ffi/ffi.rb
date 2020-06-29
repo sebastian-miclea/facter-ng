39962c05834bf40b063c4ca7d04bab58c7733cc0
Sebastian Miclea <sebastian.miclea@puppet.com>
2020-06-29 12:35:10 +0300

    Fixes 2

--- lib/resolvers/solaris/ffi/ffi.rb
+++ lib/resolvers/solaris/ffi/ffi.rb
@@ -7,13 +7,11 @@ require_relative 'functions.rb'
 module Facter
   module Resolvers
     module Solaris
-      module FFI
-        SIOCGLIFNUM = -1072928382
-        SIOCGLIFCONF = -1072666203
-        AF_INET = 2
-        AF_UNSPEC = 0
-        SOCK_DGRAM = 1
-      end
+      SIOCGLIFNUM = -1072928382
+      SIOCGLIFCONF = -1072666203
+      AF_INET = 2
+      AF_UNSPEC = 0
+      SOCK_DGRAM = 1
     end
   end
 end
