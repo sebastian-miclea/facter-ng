8ca90401d5ac13c38b69def604324cd4ee765c10
Sebastian Miclea <sebastian.miclea@puppet.com>
2020-06-30 15:55:46 +0300

    Cleanup

--- lib/resolvers/solaris/ffi/structs.rb
+++ lib/resolvers/solaris/ffi/structs.rb
@@ -2,69 +2,61 @@ module Facter
   module Resolvers
     module Solaris
 
-        class SockaddrStorage < FFI::Struct
-          layout	:sa_family, :uint16,
-	    						:pad, [:char, 240]
-        end
+      class SockaddrStorage < FFI::Struct
+        layout  :ss_family, :uint16,
+                :pad, [:char, 240]
+      end
 
-	class Sockaddr < FFI::Struct
-          layout(
-            :sa_family, :sa_family_t,
-            :sa_data, [:char, 14]
-          )
-	end
+      class Sockaddr < FFI::Struct
+        layout  :sa_family, :sa_family_t,
+                :sa_data, [:char, 14]
 
-        class Lifnum < FFI::Struct
-                layout :family, :uint,
-                        :flags, :int,
-                        :count, :int
-        end
+      end
 
-	class Arpreq < FFI::Struct
-		layout 	:arp_pa, Sockaddr,
-						:arp_ha, Sockaddr,
-						:arp_flags, :int
+      class Lifnum < FFI::Struct
+        layout  :lifn_family, :sa_family_t,
+                :lifn_flags, :int,
+                :lifn_count, :int
+      end
 
-	end
+      class Arpreq < FFI::Struct
+        layout  :arp_pa, Sockaddr,
+                :arp_ha, Sockaddr,
+                :arp_flags, :int
 
-        class Lifru1 < FFI::Union
-          layout :addrlen, :int,
-           :ppa, :size_t
-        end
+      end
 
-        class Lifru < FFI::Union
-          layout :addr, SockaddrStorage,
-           :dstaddr, SockaddrStorage,
-           :broad_addr, SockaddrStorage
-          # :token, Sockaddr,
-          # :index, :int,
-          # :flags, :uint64_t,
-          #  :metric, :int
-          # :mtu, :uint_t
+      class Lifru1 < FFI::Union
+        layout  :lifru_addrlen, :int,
+                :lifru_ppa, :uint_t
+      end
 
-        end
+      class Lifru < FFI::Union
+        layout  :lifru_addr, SockaddrStorage,
+                :lifru_dstaddr, SockaddrStorage,
+                :lifru_broadaddr, SockaddrStorage,
+                :lifru_token, SockaddrStorage,
+                :lifru_subnet, SockaddrStorage
+      end
 
-        class Lifreq < FFI::Struct
-          layout  :name, [:char, 32],
-		  :lifru1, Lifru1,
-		  :lifru, Lifru,
-            :pad, [:char, 96]
-        end
+      class Lifreq < FFI::Struct
+        layout  :name, [:char, 32],
+                :lifru1, Lifru1,
+                :lifru, Lifru,
+                :pad, [:char, 96]
+      end
 
-        class Lifconf < FFI::Struct
-          layout :family, :uint,
-            :flags, :int,
-            :len, :int,
-          # :buf, :caddr_t,
-            :buf, :pointer
-          # :lifreq, Lifreq
+      class Lifconf < FFI::Struct
+        layout  :family, :uint,
+                :flags, :int,
+                :len, :int,
+                :buf, :pointer
+      end
 
-        end
-
-        class Lifcu < FFI::Union
-          layout :lifcu_buf, :caddr_t,
-            :lifreq, Lifreq
-        end
+      class Lifcu < FFI::Union
+        layout  :lifcu_buf, :caddr_t,
+                :lifreq, Lifreq
+      end
     end
   end
 end
